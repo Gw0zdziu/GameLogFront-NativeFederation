f03fbd01c5b89b4f1abfb380f5a3d584a35f5d82
Gw0zdziu <gw0zdziu09@gmail.com>
2026-07-10 18:41:44 +0200

    feat(auth): Simplify app component placeholder template and add login button functionality

--- gamelogfront/projects/auth/src/app/app.component.ts
+++ gamelogfront/projects/auth/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import {Component, inject, OnInit, signal} from '@angular/core';
+import {ActivatedRoute, Router, RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'auth';
+export class AppComponent implements OnInit {
+  router = inject(Router)
+  private route = inject(ActivatedRoute);
+
+
+  ngOnInit() {
+    this.route.url.subscribe({
+      next: (url) => {
+        console.log(url);
+      }
+    })
+  }
+
+  click(){
+    this.router.navigate(['login'], {
+      relativeTo: this.route
+    });
+  }
 }
