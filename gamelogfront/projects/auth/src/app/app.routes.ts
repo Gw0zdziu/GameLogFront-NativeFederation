622273a533f8947b7e124d01c904696abea88bdf
Gw0zdziu <gw0zdziu09@gmail.com>
2026-07-10 18:42:45 +0200

    feat(auth): Add routing configuration for AppComponent and LoginComponent

--- gamelogfront/projects/auth/src/app/app.routes.ts
+++ gamelogfront/projects/auth/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent),
+  }
+];
